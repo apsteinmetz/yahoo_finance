366be8ed62d525a7ddc50cbcdf85cf9d278b8098
Art Steinmetz <apsteinmetz@yahoo.com>
2025-04-19 18:33:46 -0400

    regress returns against curve.

--- bond_etf.R
+++ bond_etf.R
@@ -26,10 +26,9 @@ if (RELOAD) {
       select(date, ticker,price) |> 
       arrange(date) |> 
       drop_na()
-   
    save(prices_shy, file = "data/prices_shy.RData")
    
-fred_codes <- "T10Y2Y"
+fred_codes <- c("DGS2","DGS10","T10Y2Y")
 fred_raw <- fred_codes |> 
    map(~getSymbols(.x, src = "FRED", auto.assign = FALSE)) |> 
    map(as_tibble, rownames = "date")
@@ -43,20 +42,37 @@ values_fred <- fred_raw |>
    map(~mutate(.x, date = as.Date(date))) |> 
    # combine the list of data frames into one
    reduce(full_join, by = c("date", "ticker")) |> 
-   pivot_longer(cols = -c(date, ticker), names_to = "type", values_to = "price") |> 
-   select(date, ticker,price) |> 
+   pivot_longer(cols = -c(date,ticker), names_to = "type", values_to = "level") |> 
+   select(date, ticker,level) |> 
    arrange(date) |> 
+   drop_na() |> 
+   pivot_wider(names_from = ticker, values_from = level) |>
    drop_na()
-
-save(prices_shy, file = "data/prices_shy.RData")
 save(values_fred, file = "data/values_fred.RData")
 } else {
    # load the data from file
    load("data/values_fred.RData")
    load("data/prices_shy.RData")
 }
+
+values_fred <- values_fred |> 
+   # trim to match earliest date in prices
+   # rename to substitue "T" for "DGS"
+   rename_with(~str_replace(.x, "DGS", "T"), starts_with("DGS")) |>
+   mutate(T10Y2Y = T10Y2Y*100) |> 
+   # add columns for monthly changes
+   mutate(T2Y_chg = (T2-lag(T2))*100) |>
+   mutate(T10Y_chg = (T10-lag(T10))*100) |>
+   mutate(T10Y2Y_chg = T10Y2Y-lag(T10Y2Y)) |> 
+   # convert all chg to integer
+   mutate(across(ends_with("_chg"), ~round(.x, 2))) |> 
+   filter(date >= min(prices_shy$date))
+   
+   
+
 # create daily returns and volatility
 window <- 60 # rolling 3 month window
+
 returns <- prices_shy |> 
    arrange(date) |> 
    # add daily returns
@@ -71,15 +87,7 @@ returns <- prices_shy |>
    # remove rows with NA in any column
    drop_na()
 
-values_fred <- values_fred |> 
-   # trim to match earliest date in prices
-   filter(date >= min(prices_shy$date)) |> 
-   # pivot wider
-   pivot_wider(names_from = ticker, values_from = price) |> 
-   # spread in basis points
-   mutate(T10Y2Y = T10Y2Y*100)
    
-
 #  make a monthly frequency return data frame
 window <- 36 # rolling month window
 period_scale <- 12^.5
@@ -94,14 +102,17 @@ returns_monthly <- prices_shy |>
    group_by(ticker) |>
    mutate(monthly_return = price/lag(price)-1) |> 
    mutate(log_monthly_return = log(monthly_return+1)) |> 
-   drop_na() |> 
+   drop_na() |>
    # add sd of trailing 30-day monthly returns
    mutate(vol = rollapply(log_monthly_return, width = window, FUN = sd, fill = NA, align = "right")) |>
    mutate(vol = vol*period_scale) |>
-   mutate(value = cumprod(1+monthly_return)) |>
    # remove rows with NA in any column
    drop_na() |> 
-   ungroup()
+   # make sure to accumlate value from start date of returns_monthly
+   # after drop_na() so we start at 1
+   mutate(value = cumprod(1+monthly_return)) |>
+   ungroup() |> 
+   arrange(date)
 
 # pivot longer
 returns_long <- returns_monthly |> 
@@ -154,11 +165,13 @@ avg_vol <- returns_monthly_g |>
 returns_monthly |> 
    ggplot(aes(x = date, y = value, color = ticker)) +
    geom_line() +
+   geom_hline(yintercept = 1, color = "black") +
    labs(title = "Cumulative Returns",
         x = "Date",
         y = "Value") +
    theme_minimal() +
-   scale_color_manual(values = c("SHY" = "green", "IEF" = "blue", "TLT" = "red"))
+   scale_color_manual(values = c("SHY" = "green", "IEF" = "blue", "TLT" = "red")) +
+   scale_x_date(date_labels = "%Y", date_breaks = "1 year")
 
 # plot rolling volatility
 returns_monthly_g |> 
@@ -166,6 +179,7 @@ returns_monthly_g |>
    geom_line() +
    scale_y_continuous(labels = scales::percent) +
    scale_color_manual(values = c("blue","green","red")) +
+   scale_x_date(date_labels = "%Y", date_breaks = "1 year") +
    labs(title = str_glue("{window}-Month Rolling Volatility"),
         x = "Date",
         y = "Annualized Volatility") +
@@ -204,7 +218,10 @@ correlations |>
         x = "Date",
         y = "Correlation") +
    theme_minimal() +
-   scale_y_continuous(labels = scales::percent)
+   scale_y_continuous(labels = scales::percent) +
+   # show each year on x-axis
+   scale_x_date(date_labels = "%Y", date_breaks = "1 year")
+
 
 
 window = 24 # months
@@ -244,7 +261,8 @@ beta_ratios <- returns_wide |>
       .x = beta_ratio_TLT_IEF,
       .i = date,
       .f = ~mean(.x, na.rm = TRUE),
-      .before = window-1))
+      .before = window-1)) |> 
+   tail(-8) # omit first 5 observations with too few data points
    
 vol_ratios <- returns_wide |> 
    filter(item == "vol") |>
@@ -267,15 +285,19 @@ vol_ratios <- returns_wide |>
       .x = vol_ratio_TLT_IEF,
       .i = date,
       .f = ~mean(.x, na.rm = TRUE),
-      .before = window-1))
+      .before = window-1)) |> 
+   # omit first 8 observations with too few data points
+   tail(-8) |> 
+   drop_na()
 
 # plot betas
 beta_ratios |> 
-   select(date, beta_ratio_TLT_SHY, beta_ratio_TLT_SHY_ma) |> 
+   # omit first 3 observations with too few data points
+   select(date, beta_ratio_TLT_SHY, beta_ratio_IEF_SHY) |> 
    pivot_longer(-date, names_to = "fund_pair", values_to = "beta") |> 
    ggplot(aes(x = date, y = beta, color = fund_pair)) +
    geom_line() +
-   #geom_smooth()
+   scale_x_date(date_labels = "%Y", date_breaks = "1 year") +
    labs(title = str_glue("{window}-Month Rolling Beta"),
         x = "Date",
         y = "Beta") +
@@ -311,7 +333,7 @@ hedged_port|>
 # use just last 3 years
 # lag the hedge ratios by 1 month because next month's return is based on 
 # this month's hedge ratio.  We rebalance monthly.
-hedge_port_4y <- hedged_port |> 
+hedged_port_4y <- hedged_port |> 
    filter(date >= max(date)-years(4)) |>
    mutate(
       equal_hedged = SHY-IEF,
@@ -328,10 +350,88 @@ hedge_port_4y <- hedged_port |>
       cum_beta_hedged = cumprod(1+beta_hedged)
    )
    
+hedged_port_4y_t <- hedged_port_4y |> 
+   # select just "_hedged" columns
+   select(date, ends_with("_hedged")) |> 
+   select(-contains("cum")) |>
+   # left join with values_fred
+   left_join(values_fred, by = "date") |> 
+   select(-c(T2,T10)) |> 
+   pivot_longer(-c(date,starts_with("T")), names_to = "hedge_type", values_to = "hedged_return")
+   
+
+
+# scatter plot of hedged returns vs. each T change
+hedged_port_4y_t |> 
+   group_by(hedge_type) |>
+   ggplot() +
+   geom_point(aes(x = T10Y2Y_chg, y = hedged_return, color =hedge_type)) +
+   # geom_smooth(aes(x = T10T2Y_chg, y = hedged_return, color =hedge_type),method = "lm", se = FALSE) +
+   facet_wrap(~hedge_type, scales = "free") +
+   labs(title = "Hedged Returns vs. 10Y-2Y curve Changes",
+        x = "Curve Change",
+        y = "Return",
+   ) +
+   theme_minimal() +
+   scale_y_continuous(labels = scales::percent) +
+   #remove legend
+   theme(legend.position = "none") +
+   # add a horizontal line at y = 0
+   geom_hline(yintercept = 0, linetype = "dashed", color = "black") +
+   # add a vertical line at x = 0
+   geom_vline(xintercept = 0, linetype = "dashed", color = "black") +
+   geom_text(aes(x = 0, y= 0, 
+                 label = str_glue("R²: {round(cor(hedged_return,T10Y2Y_chg),2)}")
+                 ),
+                 hjust = -0.2, vjust = -1,
+                 size = 3.5,
+                 color = "blue") +
+   geom_text(aes(x = 0, y= 0, 
+              label = str_glue("β: {round(coef(lm(IEF~SHY, data = returns_wide))[2],2)}")
+              ),
+              hjust = -0.1, vjust = -3,
+              size = 3.5,
+              color = "blue")
+
+```r
+hedged_port_4y_t |> 
+   group_by(hedge_type) |>
+   ggplot() +
+   geom_point(aes(x = T10Y2Y_chg, y = hedged_return, color = hedge_type)) +
+   facet_wrap(~hedge_type, scales = "free") +
+   labs(title = "Hedged Returns vs. 10Y-2Y curve Changes",
+        x = "Curve Change", 
+        y = "Return") +
+   theme_minimal() +
+   scale_y_continuous(labels = scales::percent) +
+   theme(legend.position = "none") +
+   geom_hline(yintercept = 0, linetype = "dashed", color = "black") +
+   geom_vline(xintercept = 0, linetype = "dashed", color = "black") +
+   geom_text(data = . %>% 
+                group_by(hedge_type) %>%
+                summarise(cor = cor(hedged_return, T10Y2Y_chg)),
+             aes(x = 0, y = 0,
+                 label = str_glue("R²: {round(cor^2,2)}")
+                 ),
+             color = "black",
+             hjust = -0.2, vjust = -3,
+             size = 5)
+
+```
+
+
+
+# add a text label with the correlation
+#greek letter for beta
+# add a text label with the beta
+# greek letter for beta
+# add a text label with the beta
+
+   
 
 
 # plot hedged daily returns
-hedge_port_4y |> 
+hedged_port_4y |> 
    select(date, equal_hedged, duration_hedged, vol_hedged, beta_hedged) |>
    pivot_longer(-date) |>
    ggplot(aes(x = date, y = value, color = name,fill = name)) +
@@ -340,10 +440,11 @@ hedge_port_4y |>
         x = "Date",
         y = "Return") +
    theme_minimal() +
+   scale_x_date(date_labels = "%Y", date_breaks = "1 year") +
    scale_y_continuous(labels = scales::percent)
 
 # plot the cumulative returns
-hedge_port_4y |> 
+hedged_port_4y |> 
    select(date, cum_equal_hedged, cum_duration_hedged, cum_vol_hedged, cum_beta_hedged) |>
    pivot_longer(-date) |>
    ggplot(aes(x = date, y = value, color = name)) +
@@ -360,9 +461,10 @@ hedge_port_4y |>
 
 # plot rolling hedge ratios
 
-x_text_pos <- max(hedged_port$date) - months(36)
+x_text_pos <- max(hedged_port$date) - months(12)
+y_text_pos <- 6
 
-hedged_port |> 
+hedged_port_4y |> 
    select(date, beta_ratio_IEF_SHY, vol_ratio_IEF_SHY) |>
    pivot_longer(cols = c(beta_ratio_IEF_SHY, vol_ratio_IEF_SHY), 
                 names_to = "hedge_ratio", values_to = "value") |> 
@@ -371,23 +473,24 @@ hedged_port |>
    scale_color_manual(values = palette()[3:5]) +
    annotate("point", x = max(hedged_port$date), y = eff_dur_IEF/eff_dur_SHY, 
             color = palette()[6], size = 4) +
-   annotate("text", x = x_text_pos, y = 12,
+   annotate("text", x = x_text_pos, y = y_text_pos,
             label = str_glue("Duration Ratio: {round(eff_dur_IEF/eff_dur_SHY,2)}"),
             color = palette()[6], hjust = -.1, size = 5) +
-   annotate("text", x = x_text_pos, y = 10,
+   annotate("text", x = x_text_pos, y = y_text_pos + 1,
             label = str_glue("Beta Ratio: {round(tail(hedged_port$beta_ratio_IEF_SHY,1),2)}"),
             color = palette()[3], hjust = -.1, size = 5) +
-   annotate("text", x = x_text_pos, y = 8, 
+   annotate("text", x = x_text_pos, y = y_text_pos + 2, 
             label = str_glue("Vol Ratio: {round(tail(hedged_port$vol_ratio_IEF_SHY,1),2)}"),
             color = palette()[4], hjust = -.1, size = 5) +
-   geom_line(aes(x = date, y = T10Y2Y/50), data = values_fred, 
+   geom_line(aes(x = date, y = T10Y2Y/50), data = filter(values_fred,date >= min(hedged_port_4y$date)),
              color = "black", linetype = "dashed", inherit.aes = FALSE) +
    scale_y_continuous(breaks = -1:20,sec.axis = sec_axis(~.*50, name = "10Y-2Y Spread")) +
+   scale_x_date(date_labels = "%Y", date_breaks = "1 year") +
    # increase the  y-axis major tick mark size
-   labs(title = str_glue("{window}-Month Rolling Hedge Ratios SHY/IEF"),
+   labs(title = str_glue("{window}-Month Rolling Hedge Ratios SHY/IEF\nHedge Ratios Are Fairly Stable"),
         x = "Date", 
         y = "Hedge Ratio",
-        subtitle = "Duration ratio uses only most recent published number") +
+        subtitle = "Note: Duration ratio uses only most recent published number") +
    theme_minimal()
 
 
